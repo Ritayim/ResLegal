e9f3d2d4633edb96472f12fc9a501767ad0c8162
pichsorita <pichsoritayim@gmail.com>
2025-10-30 23:51:58 -0700

    Add SideBar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavSideBar from './components/NavSideBar'
+import { MantineProvider } from '@mantine/core'
+
+function App() {
+  return (
+      <div className="app-layout">
+        <NavSideBar />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/NavSideBar.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { TbHomeFilled, TbBriefcaseFilled, TbUserCircle  } from 'react-icons/tb'
+import { Stack, UnstyledButton } from '@mantine/core';
+import styles from '../styles/NavSideBar.module.css';
+
+const icons = [
+    { icon: TbHomeFilled, label: 'Home' },
+    { icon: TbBriefcaseFilled, label: 'Matter' }
+];
+
+
+function SideBar({ icon: Icon, label, onClick, isActive }) {
+    return (
+        <UnstyledButton onClick={onClick} data-active={isActive || undefined} className={styles.link}>
+            <Icon size={20} stroke={1.5} />
+        </UnstyledButton>
+    );
+}
+
+export default function NavSideBar() {
+    const [active, setActive] = useState('Home');
+    const items = icons.map((item) => (
+        <SideBar
+            key={item.label}
+            icon={item.icon}
+            label={item.label}
+            onClick={() => setActive(item.label)}
+            isActive={item.label === active}
+        />
+    ));
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navbarMain}>
+                <Stack width="100%">
+                    {items}
+                </Stack>
+            </div>
+
+            <Stack justify="center" gap={0} style={{ marginBottom: '1rem' }}>
+                <SideBar 
+                    icon={TbUserCircle} 
+                    label="Profile" 
+                />
+            </Stack>
+        </nav>
+    );
+}
